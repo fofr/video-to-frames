9be3d2f7eab14f9a1e5588e95146efcf10a11ad0
Paul Hayes <fofr@users.noreply.github.com>
2023-06-09 13:26:27 +0100

    Use /tmp directory
    Remove unused python packages

--- predict.py
+++ predict.py
@@ -4,20 +4,17 @@ import subprocess
 import os
 
 class Predictor(BasePredictor):
-    def setup(self):
-        """Set up the directory to store frames"""
-        os.makedirs("./frames", exist_ok=True)
-
     def predict(self,
           video: Path = Input(description="Video input")
     ) -> List[Path]:
         """Run ffmpeg to split the video into frames"""
-        command = f"ffmpeg -i {video} -vf fps=1 ./frames/out%03d.png"
+        os.makedirs("/tmp/frames", exist_ok=True)
+        command = f"ffmpeg -i {video} -vf fps=1 /tmp/frames/out%03d.png"
         subprocess.run(command, shell=True, check=True)
 
         # Get the paths to the frames
-        frame_files = sorted(os.listdir("./frames"))
-        frame_paths = [Path(os.path.join("./frames", frame_file)) for frame_file in frame_files]
+        frame_files = sorted(os.listdir("/tmp/frames"))
+        frame_paths = [Path(os.path.join("/tmp/frames", frame_file)) for frame_file in frame_files]
 
         # Return the paths to the frames
         return frame_paths
