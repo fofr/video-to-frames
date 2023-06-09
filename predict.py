9a5efc46fdfdbbe894e633e5ea5edd42f0a48ba7
Paul Hayes <fofr@users.noreply.github.com>
2023-06-09 13:41:20 +0100

    Add an optional fps argument

--- predict.py
+++ predict.py
@@ -1,15 +1,23 @@
 from cog import BasePredictor, Input, Path
-from typing import List
+from typing import List, Optional
 import subprocess
 import os
 
 class Predictor(BasePredictor):
     def predict(self,
-          video: Path = Input(description="Video input")
+                video: Path = Input(description="Video input"),
+                fps: int = Input(description="Frames per second", default=None)
     ) -> List[Path]:
         """Run ffmpeg to split the video into frames"""
         os.makedirs("/tmp/frames", exist_ok=True)
-        command = f"ffmpeg -i {video} -vf fps=1 /tmp/frames/out%03d.png"
+
+        # If fps is provided, use it in the ffmpeg command
+        if fps is not None:
+            command = f"ffmpeg -i {video} -vf fps={fps} /tmp/frames/out%03d.png"
+        else:
+            # Default to showing every frame
+            command = f"ffmpeg -i {video} /tmp/frames/out%03d.png"
+
         subprocess.run(command, shell=True, check=True)
 
         # Get the paths to the frames
